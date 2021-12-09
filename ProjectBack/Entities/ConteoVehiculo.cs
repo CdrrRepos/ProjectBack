9fe4f83e0141ddbe628f7d148d384fa0ef91b8ec
CRISTIAN <cristiand.ruizr@utadeo.edu.co>
2021-12-08 23:51:52 -0500

    Cambio para apuntar al esquema

--- ProjectBack/Entities/ConteoVehiculo.cs
+++ ProjectBack/Entities/ConteoVehiculo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProjectBack.Entities
 {
+    [Table("ConteoVehiculo", Schema = "Cristian")]
     public class ConteoVehiculo : VehiculoModel
     {
         public int cantidad { get; set; }
--- ProjectBack/Entities/RecaudoVehiculo.cs
+++ ProjectBack/Entities/RecaudoVehiculo.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace ProjectBack.Entities
 {
+    [Table("RecaudoVehiculo", Schema = "Cristian")]
     public class RecaudoVehiculo : VehiculoModel
     {
         public int valorTabulado { get; set; }
